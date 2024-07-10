e67e90dd1c0612f332a755325f8c2b85dcf62fb9
Uzair-Raza <uzairraza0@gmail.com>
2024-07-11 01:15:11 +0500

    update

--- Admin Dashboard- Dash Reality/app.js
+++
@@ -1,7 +0,0 @@
-function openMenu(){
-   document.getElementById("nav-col-links").classList.toggle("nav-col-links");
-}
-
-function textAppear(){
-   document.getElementById("section-1").classList.add("section-1");
-}
--- Admin Dashboard- Dash Reality/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root";
-$pw = "";
-$db = "adlisting";
-
-$conn = mysqli_connect($host, $user, $pw, $db) or die ("Connection failed :" . mysqli_connect_error());
-?>
